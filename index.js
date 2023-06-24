48f59c3b62fc82f860000e1e3e03be656a663eb1
jeff <jeff.etale@student.moringaschool.com>
2023-06-24 17:15:00 +0300

    Displayed the animal details and added initial animations when an animal is picked by the user

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
+const animalList = document.querySelector('.animal-list ul');
+const animalDetails = document.querySelector('.animal-details');
+const animalImage = document.querySelector('.animal-image');
+const animalName = document.querySelector('.animal-info h3');
+const votesCount = document.querySelector('.votes-count');
+const voteButton = document.querySelector('.vote-button');
 
-  const animalList = document.querySelector('.animal-list ul');
-  const animalDetails = document.querySelector('.animal-details');
-  const animalImage = document.querySelector('.animal-image');
-  const animalName = document.querySelector('.animal-info h3');
-  const votesCount = document.querySelector('.votes-count');
-  const voteButton = document.querySelector('.vote-button');
-
-  // Fetch data from db.json
-  fetch('http://localhost:3000/characters')
-    .then(response => response.json())
-    .then(data => {
-      // Display animal names in the list
-      data.forEach(animal => {
-        const listItem = document.createElement('li');
-        listItem.textContent = animal.name;
-        listItem.addEventListener('click', () => {
-          displayAnimalDetails(animal);
-        });
-        animalList.appendChild(listItem);
-      });
-    })
-    .catch(error => console.error('Error fetching data:', error));
-
+// Fetch data from db.json
+fetch('http://localhost:3000/characters')
+   .then(response => response.json())
+   .then(data => {
+    // Display animal names in the list
+     data.forEach(animal => {
+       const listItem = document.createElement('li');
+       listItem.textContent = animal.name;
+       listItem.addEventListener('click', () => {
+         displayAnimalDetails(animal);
+       });
+       animalList.appendChild(listItem);
+     });
+      // Display initial animal details
+      displayAnimalDetails(data[0]);
+   })
+   .catch(error => console.error('Error fetching data:', error));
+// Function to display animal details
+function displayAnimalDetails(animal) {
+  // Update the UI with animal details
+  animalImage.innerHTML = `<img src="${animal.image}" alt="${animal.name}">`;
+  animalName.textContent = animal.name;
+  votesCount.textContent = animal.votes;
+}
+  
+   
